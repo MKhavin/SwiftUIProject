43702ef47b47355739578d796eac921bc1704d5e
MichaelK <havinmichael@gmail.com>
2022-08-07 20:03:54 +0300

    Implement demo from lesson

--- SwiftUIProject/ContentView.swift
+++ SwiftUIProject/ContentView.swift
@@ -8,9 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var counter = 0.0
+    @State private var isAlertPresented = false
+    @State private var text = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                Text("Hello, world!")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.blue)
+                    .padding()
+                    .background(.green)
+                
+                Button("Button") {
+                }
+                
+                Circle().frame(width: 30, height: 30)
+                
+                HStack {
+                    Text("Hello, world!")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.blue)
+                        .padding()
+                        .background(.green)
+                    
+                    Button("Button") {
+                    }
+                    
+                    Circle().frame(width: 30, height: 30)
+                }
+                
+                VStack {
+                    Text("\(counter)")
+                    Slider(value: $counter,
+                           in: 0...10)
+                    
+                    Button("Type on me", action: {
+                        isAlertPresented = true
+                    })
+                }
+                
+                Spacer()
+            }
+            .alert("Hello world",
+                   isPresented: $isAlertPresented) {}
+                .searchable(text: $text)
+                .frame(maxWidth: .infinity,
+                       alignment: .leading)
+                .padding(.leading, 16)
+                .padding(.trailing, 16)
+                .padding(.top, 5)
+        }
     }
 }
 
