5d2671984f62ddf4250b1ee87457be8c90c45489
MichaelK <havinmichael@gmail.com>
2022-08-07 18:58:59 +0300

    Initial Commit

---
+++ SwiftUIProject/SwiftUIProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIProjectApp.swift
+//  SwiftUIProject
+//
+//  Created by Michael Khavin on 07.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
